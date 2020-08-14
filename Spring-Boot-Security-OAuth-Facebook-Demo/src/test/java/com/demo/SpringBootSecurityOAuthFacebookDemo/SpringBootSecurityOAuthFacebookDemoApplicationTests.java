f1f28957af7e4a702ac938855c4919a186b0eb90
unknown <sysfmg@MJENA-GO-SS.i-flex.com>
2020-08-14 23:49:01 +0530

    code backup in Github

---
+++ Spring-Boot-Security-OAuth-Facebook-Demo/src/test/java/com/demo/SpringBootSecurityOAuthFacebookDemo/SpringBootSecurityOAuthFacebookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.SpringBootSecurityOAuthFacebookDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityOAuthFacebookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
